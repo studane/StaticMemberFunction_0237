3e314e9c3a5f1346203f732cc98a422f6d9fa7a6
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-04 18:53:08 +0700

    pendefinisial prosedur' diluar class

--- StaticFunction.cpp
+++ StaticFunction.cpp
@@ -30,3 +30,14 @@ public:
 
 int Mahasiswa::nim = 0;
 
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
