93fc378b65f6bef4b5d276dac2ab11b40e491794
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-04 18:37:57 +0700

    deklarasi variabel

--- StaticMember.cpp
+++ StaticMember.cpp
@@ -4,4 +4,9 @@ using namespace std;
 class Mahasiswa { //membuat class mahasiswa
 
 public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
     
