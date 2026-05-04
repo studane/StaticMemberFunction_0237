6eb0dfb4a5b266a616680aafe074a705dc1bf6d5
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-04 19:17:03 +0700

    menampilkan teks

--- chainfunctionalcall.cpp
+++ chainfunctionalcall.cpp
@@ -15,3 +15,11 @@ public:
     }
 };
 
+int main() 
+{
+    buku bukunya;
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();    //chain function calls
+    return 0;
+}
