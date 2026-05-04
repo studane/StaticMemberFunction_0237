600012f7088e7c136def184b7cec0a07cc5bfe98
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-04 19:20:48 +0700

    membuat akses dan variabel constructor dan destructor

--- Destructor.cpp
+++ Destructor.cpp
@@ -2,3 +2,28 @@
 using namespace std;
 
 class angka
+{
+private:
+    int arr;
+    int panjang;
+
+public:
+    angka(int);  // Constructor
+    ~angka();    // Destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i){ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
