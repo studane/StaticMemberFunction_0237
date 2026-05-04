0ee10698b9864f6a0dc8ac079fd2ec776b6144ae
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-04 18:48:46 +0700

    pembuatan static function

--- StaticFunction.cpp
+++ StaticFunction.cpp
@@ -13,4 +13,18 @@ public:
     void setID();
     void printAll();
 
-   
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+
+};
+
